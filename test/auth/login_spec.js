adbe6bf85b18ab146521456f4ad72caba26b79e0
Lim Afriyadi <lim.afriyadi.id@gmail.com>
2017-01-31 18:16:57 +0700

    Add initial tests

---
+++ test/auth/login_spec.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+
+describe("Authentication", () => {
+    it("should authenticate user with valid username and password", () => {
+        assert.equal(true, false);
+    });
+    it("should authenticate valid with invalid username and password pair", () => {
+        assert.equal(true, false);
+    });
+});
